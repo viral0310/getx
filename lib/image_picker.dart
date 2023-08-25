2c08f13903c517ba3d78a7b9ce9a8e2c852eccfc
viral0310 <viraltada2001@gmail.com>
2023-08-25 11:34:32 +0530

    Main

---
+++ lib/image_picker.dart
@@ -0,0 +1,52 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx/counter_controller.dart';
+
+class PickImage extends StatefulWidget {
+  const PickImage({super.key});
+
+  @override
+  State<PickImage> createState() => _PickImageState();
+}
+
+class _PickImageState extends State<PickImage> {
+  final ImagePickerController imagePickerController =
+      Get.put(ImagePickerController());
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade300,
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        centerTitle: true,
+        title: const Text(
+          'GetX Tutorial',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: Obx(
+        () => Center(
+          child: InkWell(
+            onTap: () {
+              imagePickerController.pickImage();
+            },
+            child: CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: 50,
+                backgroundImage: imagePickerController.imagePath.isNotEmpty
+                    ? FileImage(
+                        File(
+                          imagePickerController.imagePath.toString(),
+                        ),
+                      )
+                    : null),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getx/counter_controller.dart';
+import 'package:getx/image_picker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: const PickImage(),
     );
   }
 }
